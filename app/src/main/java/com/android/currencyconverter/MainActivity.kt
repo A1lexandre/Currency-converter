3e34a0c9969b757dbccf8be43dac4f4eaf2f1125
Alexandre Araújo <a1lexandre.na@gmail.com>
2022-06-07 19:02:35 -0300

    Result format change and keyboard dismiss

--- app/src/main/java/com/android/currencyconverter/MainActivity.kt
+++ app/src/main/java/com/android/currencyconverter/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.android.currencyconverter
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.lifecycle.Lifecycle
@@ -11,11 +13,10 @@ import androidx.lifecycle.repeatOnLifecycle
 import com.android.currencyconverter.data.ui.ConversionEvent
 import com.android.currencyconverter.databinding.ActivityMainBinding
 import com.android.currencyconverter.main.MainViewModel
-import com.android.currencyconverter.util.Resource
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
-import javax.inject.Inject
+import kotlin.math.round
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -32,7 +33,8 @@ class MainActivity : AppCompatActivity() {
             val amount = binding.tidAmount.text.toString()
             val from =   binding.spinFrom.selectedItem.toString()
             val to = binding.spinTo.selectedItem.toString()
-            if(!amount.isNullOrEmpty() && !from.isNullOrEmpty() && !to.isNullOrEmpty()) {
+            if(amount.isNotEmpty() && from.isNotEmpty() && to.isNotEmpty()) {
+                dismissKeyBoard()
                 viewModel.convert(amount, from, to)
             }
         }
@@ -47,7 +49,8 @@ class MainActivity : AppCompatActivity() {
                         is ConversionEvent.Loading -> binding.pbConversion.visibility = View.VISIBLE
                         is ConversionEvent.Success -> {
                             binding.pbConversion.visibility = View.GONE
-                            binding.tvResult.text = event.reponse.result.toString()
+                            binding.tvResult.text = "Resultado: ${formatResult(event.reponse.result)}\n " +
+                                    "                               data: ${formatDate(event.reponse.date)}"
                         }
                         is ConversionEvent.Failure -> {
                             binding.pbConversion.visibility = View.GONE
@@ -59,4 +62,19 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
-}
+
+    private fun formatResult(result: Double): String {
+        return (round(result * 100) / 100).toString()
+    }
+    
+    private fun formatDate(date: String): String {
+        val split = date.split("-")
+        return "${split[2]}/${split[1]}/${split[0]}"
+    }
+
+    private fun dismissKeyBoard() {
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        if (currentFocus != null)
+            imm.hideSoftInputFromWindow(this.currentFocus?.applicationWindowToken, 0)
+    }
+ }
